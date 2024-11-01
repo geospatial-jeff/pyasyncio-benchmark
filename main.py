26ac976973909adebd600f904453cf7d4b285f18
geospatial-jeff <geospatialjeff@gmail.com>
2024-10-31 19:49:25 -0600

    db path from env

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from importlib import import_module
 import sys
 import sqlite3
@@ -7,11 +8,14 @@ import sqlite3
 from benchmark.crud import insert_row, WorkerState
 
 
+DB_FILEPATH = os.getenv("DB_FILEPATH", "sqlite.db")
+
+
 def run_test(library_name: str, test_name: str):
     mod = import_module(f"benchmark.tests.{library_name}.{test_name}")
     worker_state: WorkerState = mod.main()
 
-    with sqlite3.connect("sqlite.db") as conn:
+    with sqlite3.connect(DB_FILEPATH) as conn:
         insert_row(conn, library_name, test_name, worker_state)
 
 
