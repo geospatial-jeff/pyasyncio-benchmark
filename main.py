734d9228547825f7551cab09d85cdfcae0e88312
geospatial-jeff <geospatialjeff@gmail.com>
2024-10-31 19:07:20 -0600

    reorganize tests

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from importlib import import_module
 import sys
 
 
-def run_test(test_name: str):
-    mod = import_module(f"benchmark.tests.{test_name}")
+def run_test(library_name: str, test_name: str):
+    mod = import_module(f"benchmark.tests.{library_name}.{test_name}")
     mod.main()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("library_name")
     parser.add_argument("test_name")
     args = parser.parse_args()
 
-    run_test(args.test_name)
+    run_test(args.library_name, args.test_name)
 
     # Kill the container
     sys.exit(1)
