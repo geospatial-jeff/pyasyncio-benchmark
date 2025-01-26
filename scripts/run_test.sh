ef931a272341c3adbdf07874fd6e7eeba7e9a009
Jeff Albrecht <geospatialjeff@gmail.com>
2025-01-26 11:10:03 -0700

    delete unused script

--- scripts/run_test.sh
+++
@@ -1,5 +0,0 @@
-library_name=$1
-test_name=$2
-
-docker build . -t pyasyncio-benchmark:latest --build-arg LIBRARY_NAME=$library_name --build-arg TEST_NAME=$test_name
-RUN_ID=$(uuidgen) docker compose up
