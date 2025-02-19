c735da74876b03f9e9de476a24d26ac1711713f4
Jeff Albrecht <jeff@arturo.ai>
2025-02-18 21:05:13 -0700

    refactur scheduling / worker state a bit to make it easier to implement more complex concurrency patterns (#48)
    * move worker state into scheduling
    
    * missing type hint
    
    * update tests

--- benchmark/scheduling.py
+++ benchmark/scheduling.py
@@ -1,27 +1,34 @@
 """Various ways of scheduling coroutines on an event loop."""
 
 import asyncio
-from typing import Any, Iterable, Coroutine, Generator
+from datetime import datetime
+from typing import Iterable, Coroutine
+
+from benchmark.crud import WorkerState
 
 
-async def gather(futs: Iterable[Coroutine]):
+async def gather(futs: Iterable[Coroutine]) -> WorkerState:
     """Run all coroutines, blocking until they all finish."""
-    return await asyncio.gather(*futs, return_exceptions=True)
+    # return await asyncio.gather(*futs, return_exceptions=True)
+    start_time = datetime.utcnow()
+    results = await asyncio.gather(*futs, return_exceptions=True)
+    end_time = datetime.utcnow()
+    n_failures = len([result for result in results if isinstance(result, Exception)])
+    return WorkerState(start_time, end_time, len(results), n_failures)
 
 
-async def as_completed(futs: Iterable[Coroutine]) -> Generator[Any, None, None]:
-    """Run all coroutines, returning results as they finish."""
-    for task in asyncio.as_completed(futs):
-        await task
-
-
-async def queue(futs: Iterable[Coroutine], num_workers: int = 3):
+async def queue(futs: Iterable[Coroutine], num_workers: int = 3) -> WorkerState:
     """Puts coroutines onto a queue and processes them with multiple workers"""
+    failure_count = 0
 
     async def _worker(queue):
+        global failure_count
         while True:
             fut = await queue.get()
-            await fut
+            try:
+                await fut
+            except Exception:
+                failure_count += 1
             queue.task_done()
 
     queue = asyncio.Queue()
@@ -33,11 +40,15 @@ async def queue(futs: Iterable[Coroutine], num_workers: int = 3):
         workers.append(task)
 
     # Start placing our coroutines onto the queue
+    start_time = datetime.utcnow()
+    n_tasks = 0
     for fut in futs:
         queue.put_nowait(fut)
+        n_tasks += 1
 
     # Wait until the queue is fully processed.
     await queue.join()
+    end_time = datetime.utcnow()
 
     # Cancel our worker tasks.
     for worker in workers:
@@ -45,3 +56,5 @@ async def queue(futs: Iterable[Coroutine], num_workers: int = 3):
 
     # Wait until all worker tasks are cancelled.
     await asyncio.gather(*workers, return_exceptions=True)
+
+    return WorkerState(start_time, end_time, n_tasks, failure_count)
--- benchmark/tests/httpx/cog_header.py
+++ benchmark/tests/httpx/cog_header.py
@@ -1,10 +1,8 @@
 import asyncio
-from datetime import datetime
 
 import httpx
 
 from benchmark import scheduling
-from benchmark.crud import WorkerState
 from benchmark.synchronization import semaphore
 from benchmark.clients import HttpClientConfig, create_httpx_client
 
@@ -29,18 +27,11 @@ async def run(config: HttpClientConfig, n_requests: int):
         config, base_url="https://sentinel-cogs.s3.amazonaws.com"
     ) as client:
         futures = (fut(client) for _ in range(n_requests))
-
-        # Schedule them using a gather.
-        start_time = datetime.utcnow()
         results = await scheduling.gather(futures)
-        end_time = datetime.utcnow()
-
-    n_failures = len([result for result in results if isinstance(result, Exception)])
-    return WorkerState(start_time, end_time, n_requests, n_failures)
+    return results
 
 
 def main(config: HttpClientConfig, n_requests: int):
-    # Run the script.
     return asyncio.run(run(config, n_requests))
 
 
