84b649d4c56878f5bc966721baaf3051a8e67fd3
Jeff Albrecht <geospatialjeff@gmail.com>
2025-04-23 00:31:22 -0600

    parse cog headers with cog_layers

--- benchmark/tests/aiohttp/cog_header.py
+++ benchmark/tests/aiohttp/cog_header.py
@@ -1,28 +1,42 @@
 import asyncio
+import typing
 
 import aiohttp
 import functools
+
+from cog_layers.reader.cog import open_cog
 
 from benchmark import scheduling
 from benchmark.synchronization import semaphore
 from benchmark.clients import HttpClientConfig, create_aiohttp_client
 
 
+bucket_name = "sentinel-cogs"
 key = "sentinel-s2-l2a-cogs/50/C/MA/2021/1/S2A_50CMA_20210121_0_L2A/B08.tif"
 
 
-@semaphore(500)
+@semaphore(100)
+async def send_range_aiohttp(
+    bucket: str, key: str, start: int, end: int, client: typing.Any | None = None
+):
+    r = await client.get(
+        f"https://{bucket}.s3.amazonaws.com/{key}",
+        headers={"Range": f"bytes={start}-{end}"},
+    )
+    r.raise_for_status()
+    return await r.read()
+
+
 async def fut(session: aiohttp.ClientSession):
     """Request the first 16KB of a file, simulating COG header request.
 
     Semaphore allows this function to be called 500 times concurrently
     """
-    r = await session.get(
-        f"https://sentinel-cogs.s3.amazonaws.com/{key}",
-        headers={"Range": "bytes=0-16384"},
+    await open_cog(
+        functools.partial(send_range_aiohttp, client=session),
+        bucket=bucket_name,
+        key=key,
     )
-    r.raise_for_status()
-    await r.read()
 
 
 async def run(config: HttpClientConfig, n_requests: int, timeout: int | None):
--- benchmark/tests/httpx/cog_header.py
+++ benchmark/tests/httpx/cog_header.py
@@ -1,32 +1,44 @@
 import asyncio
 import functools
+import typing
 
+from cog_layers.reader.cog import open_cog
 import httpx
 
 from benchmark import scheduling
 from benchmark.synchronization import semaphore
 from benchmark.clients import HttpClientConfig, create_httpx_client
 
-
+bucket_name = "sentinel-cogs"
 key = "sentinel-s2-l2a-cogs/50/C/MA/2021/1/S2A_50CMA_20210121_0_L2A/B08.tif"
-concurrency = 100
 
 
-@semaphore(concurrency)
+@semaphore(100)
+async def send_range_httpx(
+    bucket: str, key: str, start: int, end: int, client: typing.Any | None
+):
+    r = await client.get(
+        f"https://{bucket}.s3.amazonaws.com/{key}",
+        headers={"Range": f"bytes={start}-{end}"},
+    )
+    r.raise_for_status()
+    return r.read()
+
+
 async def fut(client: httpx.AsyncClient):
     """Request the first 16KB of a file, simulating COG header request.
 
     Semaphore allows this function to be called 500 times concurrently
     """
-    r = await client.get(f"/{key}", headers={"Range": "bytes=0-16384"})
-    r.raise_for_status()
-    r.read()
+    await open_cog(
+        functools.partial(send_range_httpx, client=client),
+        bucket=bucket_name,
+        key=key,
+    )
 
 
 async def run(config: HttpClientConfig, n_requests: int, timeout: int | None):
-    async with create_httpx_client(
-        config, base_url="https://sentinel-cogs.s3.amazonaws.com"
-    ) as client:
+    async with create_httpx_client(config) as client:
         if timeout:
             results = await scheduling.gather_with_timeout(
                 functools.partial(fut, client), n_requests, timeout
