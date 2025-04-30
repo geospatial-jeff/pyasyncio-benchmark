adb6cde4d66c366677dab231da6959527c59e1d1
Jeff Albrecht <geospatialjeff@gmail.com>
2025-04-30 10:20:21 -0600

    remove thread lock

--- benchmark/tests/rasterio/dump_tiles.py
+++ benchmark/tests/rasterio/dump_tiles.py
@@ -1,8 +1,6 @@
 import anyio
 import asyncio
 import functools
-import threading
-import concurrent.futures
 
 import rasterio
 
@@ -23,20 +21,8 @@ def task():
     with rasterio.open(f"s3://{bucket_name}/{key}") as src:
         windows = [window for _, window in src.block_windows()]
 
-        # We cannot write to the same file from multiple threads
-        # without causing race conditions. To safely read/write
-        # from multiple threads, we use a lock to protect the
-        # DatasetReader/Writer
-        read_lock = threading.Lock()
-
-        def process(window):
-            with read_lock:
-                src.read(window=window)
-
-        # We map the process() function over the list of
-        # windows.
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(process, windows)
+        for window in windows:
+            src.read(window=window)
 
 
 @semaphore(2)
